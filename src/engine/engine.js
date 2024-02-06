3c8d6ad5f1ca192a9559377e8e5e5880308c9677
Gavin Popken <gavinpopkenart@gmail.com>
2024-02-06 11:24:25 -0600

    rooms now can be locked

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,3 +1,5 @@
+// ! THE CONFIG OBJ NEEDS TO LOOK LIKE THIS
+
 const confOjb = {
     rooms: {
         '0.0': { description: "This is red room.", isOpen: 1, items: [1, 5, 7] },
@@ -7,6 +9,8 @@ const confOjb = {
     history: ['1.2', '1.3']
 }
 
+// ! --- --- --- ---
+
 export const configMove = (confObj) => {
     const checkValidRoom = (coord) => {
         if (Object.hasOwn(confObj.rooms, coord)) {
@@ -15,72 +19,56 @@ export const configMove = (confObj) => {
         return false
     }
 
-    const moveDown = () => {
-        const coords = confObj.history[confObj.history.length - 1].split('.')
-        let attemptedCoords = coords.toSpliced(1, 1, +coords[1] + 1).join('.')
+    const checkIfOpen = (coord) => {
+        if (confObj.rooms[coord].isOpen) {
+            return true
+        }
+        return false
+    }
+
+    const handleAccess = (attemptedCoords, prevCoords) => {
         if (checkValidRoom(attemptedCoords)) {
-            // if (confObj.history)
+            if (checkIfOpen(attemptedCoords)) {
                 return {
                     history: [...confObj.history, attemptedCoords],
                     room: confObj.rooms[attemptedCoords],
                 }
-        } else {
+            }
             return {
-                history: [...confObj.history, coords.join('.')],
+                history: [...confObj.history, prevCoords],
+                room: { description: 'This room is locked.' }
+            }
+        }
+        else {
+            return {
+                history: [...confObj.history, prevCoords],
                 room: { description: 'You have hit a wall.' }
             }
         }
+    }
+
+    const moveDown = () => {
+        const coords = confObj.history[confObj.history.length - 1].split('.')
+        let attemptedCoords = coords.toSpliced(1, 1, +coords[1] + 1).join('.')
+        return handleAccess(attemptedCoords, coords.join('.'))
     }
 
     const moveUp = () => {
         const coords = confObj.history[confObj.history.length - 1].split('.')
         let attemptedCoords = coords.toSpliced(1, 1, +coords[1] - 1).join('.')
-        if (checkValidRoom(attemptedCoords)) {
-            // if (confObj.history)
-                return {
-                    history: [...confObj.history, attemptedCoords],
-                    room: confObj.rooms[attemptedCoords],
-                }
-        } else {
-            return {
-                history: [...confObj.history, coords.join('.')],
-                room: { description: 'You have hit a wall.' }
-            }
-        }
+        return handleAccess(attemptedCoords, coords.join('.'))
     }
 
     const moveLeft = () => {
         const coords = confObj.history[confObj.history.length - 1].split('.')
         let attemptedCoords = coords.toSpliced(0, 1, +coords[0] - 1).join('.')
-        if (checkValidRoom(attemptedCoords)) {
-            // if (confObj.history)
-                return {
-                    history: [...confObj.history, attemptedCoords],
-                    room: confObj.rooms[attemptedCoords],
-                }
-        } else {
-            return {
-                history: [...confObj.history, coords.join('.')],
-                room: { description: 'You have hit a wall.' }
-            }
-        }
+        return handleAccess(attemptedCoords, coords.join('.'))
     }
 
     const moveRight = () => {
         const coords = confObj.history[confObj.history.length - 1].split('.')
         let attemptedCoords = coords.toSpliced(0, 1, +coords[0] + 1).join('.')
-        if (checkValidRoom(attemptedCoords)) {
-            // if (confObj.history)
-                return {
-                    history: [...confObj.history, attemptedCoords],
-                    room: confObj.rooms[attemptedCoords],
-                }
-        } else {
-            return {
-                history: [...confObj.history, coords.join('.')],
-                room: { description: 'You have hit a wall.' }
-            }
-        }
+        return handleAccess(attemptedCoords, coords.join('.'))
     }
     return { moveDown, moveUp, moveLeft, moveRight }
 }
