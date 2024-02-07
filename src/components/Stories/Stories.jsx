40f78e8551e9e0cf45abe59484f218fe95339bfa
Gavin Popken <gavinpopkenart@gmail.com>
2024-02-07 10:17:19 -0600

    client side get request set up

--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -1,7 +1,21 @@
-
+import axios from "axios"
+import { useEffect, useState} from "react"
+import { useDispatch } from "react-redux"
 
 export const Stories = () => {
+    const dispatch = useDispatch()
+    const [stories, setStories] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/stories')
+        .then(response => {
+            setStories(response.data)
+        }).catch(err => {
+            console.log(err);
+        })
+    }, [])
+
     return (
-        <p>Stories are loaded here.</p>
+        <p>{JSON.stringify(stories)}</p>
     )
 }
