cdc33bb5b4f09456f1bedf6b74f1bd8c01fd6901
Gavin Popken <gavinpopkenart@gmail.com>
2024-02-09 09:28:51 -0600

    working on delete fix

--- server/routes/stories.router.js
+++ server/routes/stories.router.js
@@ -12,13 +12,13 @@ router.get('/', rejectUnauthenticated, (req, res) => {
     WHERE story.user_id = $1;
   `
     pool.query(queryText, [req.user.id])
-    .then(result => {
-        // console.log(result.rows);
-        res.send(result.rows)
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500)
-    })
+        .then(result => {
+            // console.log(result.rows);
+            res.send(result.rows)
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500)
+        })
 });
 
 /**
@@ -32,42 +32,72 @@ router.post('/', (req, res) => {
     RETURNING id;
     `
     pool.query(queryText, [req.body.story_name, req.user.id])
-    .then(result => {
-        console.log(result);
-        res.send(result.rows[0])
-    }).catch(err => {
-        console.log(err);
-    })
+        .then(result => {
+            console.log(result);
+            res.send(result.rows[0])
+        }).catch(err => {
+            console.log(err);
+        })
 });
 
-router.delete('/:storyId', rejectUnauthenticated, (req, res) => {
-    let queryText = `
-    SELECT "user".id FROM "user"
-    JOIN story ON story.user_id = "user".id
-    WHERE story.id = $1;
-    `
-    pool.query(queryText, [req.params.storyId])
-    .then(result => {
+router.delete('/:storyId', rejectUnauthenticated, async (req, res) => {
+    // let queryText = `
+    // SELECT "user".id FROM "user"
+    // JOIN story ON story.user_id = "user".id
+    // WHERE story.id = $1;
+    // `
+    // pool.query(queryText, [req.params.storyId])
+    // .then(result => {
+    //     if (result.rows[0].id === req.user.id) {
+    //         console.log("This is allowed");
+    //         queryText = `
+    //         DELETE FROM "story"
+    //         WHERE id = $1
+    //         `
+    //         pool.query(queryText, [req.params.storyId])
+    //         .then(result => {
+    //             res.sendStatus(201)
+    //         }).catch(err => {
+    //             console.log(err);
+    //             res.sendStatus(500)
+    //         })
+    //     } else {
+    //         res.sendStatus(403)
+    //     }
+    // }).catch(err => {
+    //     console.log(err);
+    //     res.sendStatus(500)
+    // })
+    const connection = await pool.connect()
+    try {
+        await connection.query('BEGIN')
+        let queryText = `
+        SELECT "user".id FROM "user"
+        JOIN story ON story.user_id = "user".id
+        WHERE story.id = $1;
+        `
+        const result = await connection.query(queryText, [req.params.storyId])
+        // console.log(result.rows);
         if (result.rows[0].id === req.user.id) {
-            console.log("This is allowed");
             queryText = `
-            DELETE FROM "story"
-            WHERE id = $1
+            DELETE FROM "baseMode_adventures"
+            WHERE story_id = $1;
             `
-            pool.query(queryText, [req.params.storyId])
-            .then(result => {
-                res.sendStatus(201)
-            }).catch(err => {
-                console.log(err);
-                res.sendStatus(500)
-            })
+            await connection.query(queryText, [req.params.storyId])
+            queryText = `
+            DELETE FROM story 
+            WHERE id = $1;
+            `
+            await connection.query(queryText, [req.params.storyId])
+            await connection.query("COMMIT")
         } else {
             res.sendStatus(403)
         }
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500)
-    })
+    } catch (error) {
+
+    } finally {
+        connection.release()
+    }
 })
 
 module.exports = router;
